d623f22e37b698b7707be278d4692d38aa5acb4b
Fabiolfsallles <148702071+Fabiolfsallles@users.noreply.github.com>
2025-05-29 20:41:19 -0300

    Delete oficina_mecanica directory

--- oficina_mecanica/principal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Produto, OrdemDeServico, Agendamento
-
-admin.site.register(Cliente)
-admin.site.register(Produto)
-admin.site.register(OrdemDeServico)
-admin.site.register(Agendamento)
